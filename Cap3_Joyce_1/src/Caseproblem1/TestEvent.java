02434c98bce2519961e93bfa03b5cf0dd691f267
Felipesorsilv <61020704+Felipesorsilv@users.noreply.github.com>
2021-03-01 14:36:16 -0600

    Add files via upload

---
+++ Cap3_Joyce_1/src/Caseproblem1/TestEvent.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Caseproblem1;
+
+/**
+ *
+ * @author felipe
+ */
+public class TestEvent {
+    
+    public class TestEvent {
+    public static void main(String[] args) {
+        EventDemo evento = new EventDemo();
+        evento.fiesta();
+    }
+}
